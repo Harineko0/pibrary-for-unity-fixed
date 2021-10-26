7a654a0f1cce76317e2230651451e8d1bea1c442
Harineko0 <harineko0927@gmail.com>
2021-10-26 20:46:48 +0900

    [Resolve] Resolve Assembly.

--- Assets/Pibrary/Scripts/Pibrary/PibraryInstaller.cs
+++ Assets/Pibrary/Scripts/Pibrary/PibraryInstaller.cs
@@ -1,13 +1,13 @@
-using Pibrary.Config;
-using Zenject;
-
-namespace Pibrary
-{ 
-    public class PibraryInstaller : MonoInstaller
-    {
-        public override void InstallBindings()
-        {
-            // Container.Bind<IConfigLoader>().To<AddressableConfigLoader>().AsCached();
-        }
-    }
-}
+// using Pibrary.Config;
+// using Zenject;
+//
+// namespace Pibrary
+// { 
+//     public class PibraryInstaller : MonoInstaller
+//     {
+//         public override void InstallBindings()
+//         {
+//             // Container.Bind<IConfigLoader>().To<AddressableConfigLoader>().AsCached();
+//         }
+//     }
+// }
